4fe6c8dcc882c376fb528eb781cac546e8c37e26
shiwam17 <shiwam17@gmail.com>
2022-04-05 11:26:12 +0530

    rightbr and post

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import Share from '../share/Share'
-import './feed.css'
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import { Posts } from "../../dummyData";
 
 export default function Feed() {
   return (
-    <div className='feed'>
+    <div className="feed">
       <div className="feedWrapper">
-        <Share/>
+        <Share />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,92 +1,59 @@
-import { Bookmark, Chat, Event, Group, Pause, PlayCircle, PlayDisabled, QuestionAnswer, QuestionMarkRounded, RssFeed, School, VideoSettings, WorkOutline } from '@mui/icons-material'
+import { Bookmark, Chat, Event, Group, Pause, PlayCircle, PlayDisabled, QuestionAnswer, QuestionMarkRounded, RssFeed, School, VideoSettings, WorkOutline,PlayCircleFilledOutlined,HelpOutline } from '@mui/icons-material'
 import React from 'react'
 import "./sidebar.css"
+import CloseFriend from "../closeFriend/CloseFriend";
+import { Users } from '../../dummyData';
 
 export default function Sidebar() {
-  return (
-    <div className='sidebar'>
+    return (
+      <div className="sidebar">
         <div className="sidebarWrapper">
-            <ul className="sidebarList">
-                <li className="sidebarListItem">
-                    <RssFeed className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    Feed
-                    </span>
-                </li>
-                <li className="sidebarListItem">
-                    <Chat className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    chat
-                    </span>
-                </li>
-                <li className="sidebarListItem">
-                    <PlayCircle className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    video
-                    </span>
-                </li>
-                <li className="sidebarListItem">
-                    <Group className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    Group
-                    </span>
-                </li>
-                <li className="sidebarListItem">
-                    <Bookmark className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    Bookmarks
-                    </span>
-                </li>
-                <li className="sidebarListItem">
-                    <Event className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    Events
-                    </span>
-                </li>
-                <li className="sidebarListItem">
-                    <QuestionMarkRounded className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    Questions
-                    </span>
-                </li>
-                <li className="sidebarListItem">
-                    <WorkOutline className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    jobs
-                    </span>
-                </li>
-                <li className="sidebarListItem">
-                    <School className='sideBarIcon'/>
-                    <span className="sidebarListItemText">
-                    Courses
-                    </span>
-                </li>
-            </ul>
-            <button className='sidebarButton'>Show more</button>
-            <hr className='sidebarHr'/>
-            <ul className="sidebarFriendList">
-                <li className="sidebarFriend">
-                    <img src="/assets/profile2.jpeg" alt="" className="sidebarFriendImg" />
-                    <span className="sidebarFriendName">Shiwam bhargava</span>
-                </li>
-                <li className="sidebarFriend">
-                    <img src="/assets/profile2.jpeg" alt="" className="sidebarFriendImg" />
-                    <span className="sidebarFriendName">Shiwam bhargava</span>
-                </li>
-                <li className="sidebarFriend">
-                    <img src="/assets/profile2.jpeg" alt="" className="sidebarFriendImg" />
-                    <span className="sidebarFriendName">Shiwam bhargava</span>
-                </li>
-                <li className="sidebarFriend">
-                    <img src="/assets/profile2.jpeg" alt="" className="sidebarFriendImg" />
-                    <span className="sidebarFriendName">Shiwam bhargava</span>
-                </li>
-                <li className="sidebarFriend">
-                    <img src="/assets/profile2.jpeg" alt="" className="sidebarFriendImg" />
-                    <span className="sidebarFriendName">Shiwam bhargava</span>
-                </li>
-            </ul>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <RssFeed className="sidebarIcon" />
+              <span className="sidebarListItemText">Feed</span>
+            </li>
+            <li className="sidebarListItem">
+              <Chat className="sidebarIcon" />
+              <span className="sidebarListItemText">Chats</span>
+            </li>
+            <li className="sidebarListItem">
+              <PlayCircleFilledOutlined className="sidebarIcon" />
+              <span className="sidebarListItemText">Videos</span>
+            </li>
+            <li className="sidebarListItem">
+              <Group className="sidebarIcon" />
+              <span className="sidebarListItemText">Groups</span>
+            </li>
+            <li className="sidebarListItem">
+              <Bookmark className="sidebarIcon" />
+              <span className="sidebarListItemText">Bookmarks</span>
+            </li>
+            <li className="sidebarListItem">
+              <HelpOutline className="sidebarIcon" />
+              <span className="sidebarListItemText">Questions</span>
+            </li>
+            <li className="sidebarListItem">
+              <WorkOutline className="sidebarIcon" />
+              <span className="sidebarListItemText">Jobs</span>
+            </li>
+            <li className="sidebarListItem">
+              <Event className="sidebarIcon" />
+              <span className="sidebarListItemText">Events</span>
+            </li>
+            <li className="sidebarListItem">
+              <School className="sidebarIcon" />
+              <span className="sidebarListItemText">Courses</span>
+            </li>
+          </ul>
+          <button className="sidebarButton">Show More</button>
+          <hr className="sidebarHr" />
+          <ul className="sidebarFriendList">
+            {Users.map((u) => (
+              <CloseFriend key={u.id} user={u} />
+            ))}
+          </ul>
         </div>
-    </div>
-  )
-}
+      </div>
+    );
+  }
